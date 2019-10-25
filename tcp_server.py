2f77f38cb9effdce2eaa1d9f5bf27775eeae5d7c
Viktor <half04@yandex.ru>
2019-10-25 09:51:09 +0000

    Step 1.6 Assignment 1

--- tcp_server.py
+++ tcp_server.py
@@ -1,13 +1,11 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('127.0.0.1', 1235))
+s.bind(('0.0.0.0', 2222))
 s.listen(10)
 while True:
     conn, addr = s.accept()
     while True:
         data = conn.recv(1024)
         if data == "close" or not data: break
-        print "received data:", data, "@@@"
         conn.send(data)
     conn.close()
-    if data == "close": break
